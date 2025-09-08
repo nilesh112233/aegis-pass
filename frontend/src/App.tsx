705b206500879b82245aee9302b73acf5ef3d017
nilesh112233 <148685313+nilesh112233@users.noreply.github.com>
2025-09-08 12:39:52 +0530

    Initial project setup with backend and frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+
+function App() {
+
+  return (
+    <>
+      <div className='dark:bg-gray-900 dark:text-white text-gray-900 text-center'>
+        <h1>Aegis-Pass</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
